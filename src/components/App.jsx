30289bd9bafb9a3b4995c9ad2e08512d9916f29e
LiubovRybak <rybak94@ukr.net>
2023-03-21 20:11:54 +0100

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import user from '../user.json';
+
+// export const App = () => {
+//   return (
+//     <>
+//   <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   followers={user.stats.followers}
+//   views={user.stats.views}
+//   likes={user.stats.likes}
+// />
+//     </>
+//   )
+// }
