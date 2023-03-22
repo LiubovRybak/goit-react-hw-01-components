5072e43849782e6c109301e1993b019a06d8a3ab
LiubovRybak <rybak94@ukr.net>
2023-03-22 19:12:42 +0100

    third

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-// import user from '../user.json';
+import user from '../user.json';
 
-// export const App = () => {
-//   return (
-//     <>
-//   <Profile
-//   username={user.username}
-//   tag={user.tag}
-//   location={user.location}
-//   avatar={user.avatar}
-//   followers={user.stats.followers}
-//   views={user.stats.views}
-//   likes={user.stats.likes}
-// />
-//     </>
-//   )
-// }
+export const App = () => {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </>
+  );
+};
