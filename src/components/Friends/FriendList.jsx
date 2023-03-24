2fb4e3468055811a4615a746b65794762a15a913
LiubovRybak <rybak94@ukr.net>
2023-03-24 18:33:46 +0100

    hw

--- src/components/Friends/FriendList.jsx
+++ src/components/Friends/FriendList.jsx
@@ -4,7 +4,7 @@ import { FriendListItem } from './FriendListItem';
 export const FriendList = ({ friends }) => {
   return (
     <ul className={css.friendList}>
-      <FriendListItem friends={friends} />;
+      <FriendListItem friends={friends} />
     </ul>
   );
 };
--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -5,9 +5,9 @@ export const FriendListItem = ({ friends }) => {
   return friends.map(({ name, avatar, isOnline, id }) => (
     <li className={css.item} key={id}>
       {isOnline ? (
-        <span className={css.statusBlue}></span>
+        <span className={css.statusGreen}></span>
       ) : (
-        <span className={css.statusYellow}></span>
+        <span className={css.statusRed}></span>
       )}
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
